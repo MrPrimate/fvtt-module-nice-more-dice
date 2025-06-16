0404e570c1939e2ad6112be8b80c6a0a087ec2db
Jack Holloway <jack@mrprimate.co.uk>
2025-06-16 12:17:15 +0100

    add package

--- .github/workflows/build-module-json.js
+++ .github/workflows/build-module-json.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-const version = JSON.parse(fs.readFileSync('./nice-more-dice/package.json', 'utf8')).version;
+const version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
 
 var mod = JSON.parse(fs.readFileSync('./nice-more-dice/module-template.json', 'utf8'));
 
